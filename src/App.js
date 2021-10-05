a182f8754cac38ab2c99753de550980e03139ecb
yoshiyuki-nagata <yoshiyuki_nagata@rosso-tokyo.co.jp>
2021-10-06 00:34:45 +0900

    learn useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
+import React, { useState } from "react";
+
 function App() {
+  // 配列の１個目がカウント、２個目は１個目を操作する権限のある関数、慣習的にsetHogeとする事が多い
+  const [count, setCount] = useState(0)
+
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(count - 1)
+  const reset = () => setCount(0)
+  const double = () => setCount(count * 2)
+  const divede = () => setCount(div =>{
+    if (div !== 0){
+      return div / 2 
+    }else{
+      alert('dont divide 0')
+      return div
+    }
+  })
+
+  // reactの仕様上、retun内に複数のトップレベルタグを置けない
+  // <>でトップレベルを設定すれば解決する、divタグで囲っても良いが不要なdivタグが増えるので非推奨 
   return (
-    <div>
-      this is a template
-    </div>
+    <>
+      <div>count: {count}</div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+        <button onClick={double}>x2</button>
+        <button onClick={divede}>/1</button>
+      </div>
+
+      <div>
+        <button onClick={reset}>Reset</button>
+      </div>
+    </>
   );
 }
 
